2c18581ce4384119204af0566320549f85c10b8e
rfoeckin <rfoeckin@ubuntu.ubuntu-domain>
2009-07-16 09:51:26 -0400

    Resolving conflicts

--- mousetrap/src/ocvfw/idm/color.py
+++ mousetrap/src/ocvfw/idm/color.py
@@ -270,7 +270,7 @@ class Module(object):
 
         temphue = self.rgb2hue(float(self.cfg.get("color", "red")), float(self.cfg.get("color", "green")), float(self.cfg.get("color", "blue")))
 
-        hrange = self.cfg.hrange / 2
+        hrange = int(self.cfg.get("color","hrange")) / 2
         self.hmin.value = int(max(temphue - hrange, 0))
         self.hmax.value = int(min(temphue + hrange, 180))
 
@@ -304,7 +304,7 @@ class Module(object):
 
             temphue = self.rgb2hue(float(self.cfg.get("color", "red")), float(self.cfg.get("color", "green")), float(self.cfg.get("color", "blue")))
 
-            hrange = self.cfg.hrange / 2
+            hrange = int(self.cfg.get("color","hrange")) / 2
             self.hmin.value = int(max(temphue - hrange, 0))
             self.hmax.value = int(min(temphue + hrange, 180))
             print str(self.hmin.value) + ", " + str(self.hmax.value)
