b722e9acb32e804ed868f751a9f7af3d1229370b
Ryan <rgee@wesleyan.edu>
2009-07-20 14:19:40 -0400

    Changed the debug level conditionals. >= 10 will show debug info

--- mousetrap/src/ocvfw/idm/color.py
+++ mousetrap/src/ocvfw/idm/color.py
@@ -160,7 +160,7 @@ class Module(object):
         # by the idm because the Capture syncs the image asynchronously (See dev/camera.py)
         self.cap = Capture(async=False, idx=cam, backend="OcvfwCtypes")
         
-        if(self.debugLevel == 10):
+        if(self.debugLevel >= 10):
             co.hg.cvNamedWindow( "Histogram", 1 )
             co.hg.cvNamedWindow( "Mask", 1 )
 
@@ -285,7 +285,7 @@ class Module(object):
             color = self.hsv2rgb(i*180./self.hdims)            co.cv.cvRectangle( self.histimg, co.cv.cvPoint(i*bin_w,self.histimg.height),
                              co.cv.cvPoint((i+1)*bin_w,self.histimg.height - val),
                              color, -1, 8, 0 )
-        if(self.debugLevel == 10):
+        if(self.debugLevel >= 10):
             co.hg.cvShowImage( "Histogram", self.histimg )
 
     def get_capture(self):
@@ -389,7 +389,7 @@ class Module(object):
             co.cv.cvXorS( self.image, co.cv.cvScalarAll(255), self.image )
             co.cv.cvResetImageROI( self.image )
         
-        if(self.debugLevel == 10):
+        if(self.debugLevel >= 10):
             co.hg.cvShowImage( "Mask", self.mask)
         
         self.cap.color("rgb", channel=3, copy=True)
